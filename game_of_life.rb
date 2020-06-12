e47f8c35dba3e61076396d63975f7d1206a76721
Ana Arreola <laurarreola.ana@gmail.com>
2020-06-11 23:22:28 -0500

    Creating branch sesion-01

--- game_of_life.rb
+++ game_of_life.rb
@@ -4,11 +4,17 @@ Una celda viva que tenga menos de dos vecinos vivos, muere (despoblación)
 Una celda viva con dos o tres celdas vivas como vecinos vivos, pasa a la siguiente generación.
 Cualquier celda muerta que tenga exactamente tres vecinos vivos, vuelve a la vida (reproducción)
 Cualquier celda viva con más de tres vecinos vivos, muere (sobrepoblación) 
-
-Los array pueden ser vectores o matrices
-Arrays de matrices en ruby
-names = ['Ana', 'Andrea', 'Karen']
-celulas = [["true","false"],["true","false"]]
-lenguajes.each do |parámetro|
+1.Crear/declarar una matriz de 10 x 10 
+2.Llenar autmáticamente la matriz con valores aleatorios 
+3. Iterar en cada uno de los valores de la matriz
 =end
+m= Matrix.build(10){rand(0..1)}
+ puts
+end
 
+puts m
+
+require 'matrix'
+m.each do |i|
+    puts
+end
