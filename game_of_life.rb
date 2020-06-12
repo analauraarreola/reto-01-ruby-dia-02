2865a6423593c3300ebca03f6422d60dea9a15d4
Ana Arreola <laurarreola.ana@gmail.com>
2020-06-11 23:15:17 -0500

    Creating branch master

---
+++ game_of_life.rb
@@ -0,0 +1,14 @@
+=begin= 
+Reglas
+Una celda viva que tenga menos de dos vecinos vivos, muere (despoblación)
+Una celda viva con dos o tres celdas vivas como vecinos vivos, pasa a la siguiente generación.
+Cualquier celda muerta que tenga exactamente tres vecinos vivos, vuelve a la vida (reproducción)
+Cualquier celda viva con más de tres vecinos vivos, muere (sobrepoblación) 
+
+Los array pueden ser vectores o matrices
+Arrays de matrices en ruby
+names = ['Ana', 'Andrea', 'Karen']
+celulas = [["true","false"],["true","false"]]
+lenguajes.each do |parámetro|
+=end
+
